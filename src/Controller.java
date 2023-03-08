3590e2bd8b2e7e29bdf418b808f8cfe3a41a25a7
Hoejhus <mikk57k0@stud.kea.dk>
2023-03-08 10:06:16 +0100

    newvehicle

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,29 @@
 package src;
 
 public class Controller {
+    Menu menu = new Menu();
+    public static void main(String[] args) {
+        new Controller().run();
+    }
+
+    public void run() {
+        int choice;
+        menu.menu.printMenu();
+        choice = menu.ui.readChoiceInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("You've selected: New Rental Contract.");
+                break;
+            case 2:
+                System.out.println("You've selected: Add new Vehicle. ");
+                menu.createNewVehicle();
+                break;
+            case 3:
+                System.out.println("You selected Strawberry");
+                break;
+        }
+    }
 
 }
 
--- src/Menu.java
+++ src/Menu.java
@@ -1,20 +1,53 @@
 package src;
 
+import src.entities.Car;
+
 import java.util.Scanner;
 
 public class Menu {
     GenericMenu menu = new GenericMenu("---[Kailau Car Rental]---", "Make your choice: ",new String[] {"1. New Rental Contract", "2. Add New Vehicle", "3. Remove Vehicle"});
     UI ui = new UI();
+    Scanner in = new Scanner(System.in);
 
-    public void run() {
-        int choice;
-        menu.printMenu();
-        choice = ui.readChoiceInt();
+    public void createNewVehicle() {
+        System.out.println("You've selected to Create new Vehicle:");
+        String brand;
+        String model;
+        String fuelType;
+        String registrationYear;
+        String type;
+        String isRentedChoice;
+        boolean approvedChoice = false;
+        boolean isRented = false;
 
-        switch (choice) {
-            case 1 -> System.out.println("You selected Apple");
-            case 2 -> System.out.println("You selected Banana ");
-            case 3 -> System.out.println("You selected Strawberry");
+        System.out.print("Enter Brand of Vehicle: ");
+        brand = in.nextLine();
+        System.out.print("Enter Model of Vehicle: ");
+        model = in.nextLine();
+        System.out.print("Enter Fuel Type of Vehicle: ");
+        fuelType = in.nextLine();
+        System.out.print("Enter Registration Year of Vehicle: ");
+        registrationYear = in.nextLine();
+        System.out.print("Enter Type of Vehicle: ");
+        type = in.nextLine();
+        System.out.print("Is Car already rented? (Yes)(No): ");
+        isRentedChoice = in.nextLine();
+        isRentedChoice = isRentedChoice.toLowerCase();
+
+        while (!approvedChoice) {
+            if (isRentedChoice.equals("yes") || isRentedChoice.equals("no")) {
+                if (isRentedChoice.equals("yes")) {
+                    isRented = true;
+                    approvedChoice = true;
+                } else if (isRentedChoice.equals("no")) {
+                    isRented = false;
+                    approvedChoice = true;
+                } else {
+                    System.out.println("Unknown Input, try again (Yes or No)");
+                }
+            }
         }
+
+        Car car = new Car(brand, model, fuelType, registrationYear, type, isRented);
     }
 }
