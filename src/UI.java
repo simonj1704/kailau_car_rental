bdd2bc47c085b11d18fc777dac132e8823614f59
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2023-03-08 11:53:21 +0100

    Added addCustomer method + ui stuff

--- src/UI.java
+++ src/UI.java
@@ -20,4 +20,25 @@ public class UI {
         } while (!validChoice);
         return choice;
     }
+    public void printLine(String message){
+        System.out.println(message);
+    }
+    public void print(String message){
+        System.out.print(message);
+    }
+
+    public String readString(){
+        return in.nextLine();
+    }
+
+    public String readString(String message){
+        print(message);
+        return readString();
+    }
+
+    public int readInt(){
+        int number = in.nextInt();
+        in.nextLine(); //scanner bug
+        return number;
+    }
 }
